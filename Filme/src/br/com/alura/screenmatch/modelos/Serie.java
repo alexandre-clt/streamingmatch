df68dff34589f97e322f1a6632812e5f706a159c
Raimundo Alexandre <alexandre.fsantos08@gmail.com>
2025-11-18 18:40:09 -0300

    Atualização, criação da classe série

---
+++ Filme/src/br/com/alura/screenmatch/modelos/Serie.java
@@ -0,0 +1,29 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Serie extends Titulo{
+
+    private int temporadas;
+    private boolean ativa;
+    private int episodiosPorTemporada;
+    private int minutosPorEpisodio;
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public void setAtiva(boolean ativa) {
+        this.ativa = ativa;
+    }
+
+    public void setEpisodiosPorTemporada(int episodiosPorTemporada) {
+        this.episodiosPorTemporada = episodiosPorTemporada;
+    }
+
+    public void setMinutosPorEpisodio(int minutosPorEpisodio) {
+        this.minutosPorEpisodio = minutosPorEpisodio;
+    }
+    @Override
+    public int getDuracaoEmMinutos() {
+        return temporadas * episodiosPorTemporada * minutosPorEpisodio;
+    }
+}
